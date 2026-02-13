b131441d8a2675eba55f55601e28f79a3731634d
JK76761 <156045117+JK76761@users.noreply.github.com>
2026-02-13 23:39:52 +1000

    dwdwd

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function openLetter() {
+  document.getElementById("landing").classList.add("hidden");
+  document.getElementById("timeline").classList.remove("hidden");
+  revealCards();
+}
+
+function goToFinal() {
+  document.getElementById("timeline").classList.add("hidden");
+  document.getElementById("final").classList.remove("hidden");
+}
+
+function heartExplosion() {
+  alert("I love you more than anything ❤️");
+}
+
+/* 카드 순차 fade */
+function revealCards() {
+  const cards = document.querySelectorAll(".fade");
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      card.classList.add("show");
+    }, index * 800);
+  });
+}
